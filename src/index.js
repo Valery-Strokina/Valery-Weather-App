94d61a0c28663a5e88f5827428675721133966c6
Valery-Strokina <ms.nanyeva@gmail.com>
2022-07-05 13:10:45 +0300

    fixed forecast

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7 && index > 0) {
       forecastHTML =
         forecastHTML +
         `<div class="col-2 sat">
