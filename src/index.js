e6d5644782a843b3d5146cc03dc8da9a3df59df7
Valery-Strokina <ms.nanyeva@gmail.com>
2022-07-05 12:44:56 +0300

    Display the forecat data using the API

--- src/index.js
+++ src/index.js
@@ -115,24 +115,39 @@ currentButton.addEventListener("click", getCurrentLocation);
 
 searchCity("London");
 
-function displayForecast(response) {
-  console.log(response.data.daily);
-  let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2 sat">
-              <div class="weather-forecast-date">${day}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 sat">
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
               <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=" "
                 width="42"
               />
-              <div class="weather-forecast-temperatures">20°C</div>
+              <div class="weather-forecast-temperatures">${Math.round(
+                forecastDay.temp.day
+              )}°C</div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
